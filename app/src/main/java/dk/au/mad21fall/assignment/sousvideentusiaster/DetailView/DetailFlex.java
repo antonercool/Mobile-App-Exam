f75d2477e902c4a702d5b5d9538c7920154d10f8
Peter windows <201508876PMH@gmail.com>
2021-12-03 10:45:43 +0100

    added commenting function

--- app/src/main/java/dk/au/mad21fall/assignment/sousvideentusiaster/DetailView/DetailFlex.java
+++ app/src/main/java/dk/au/mad21fall/assignment/sousvideentusiaster/DetailView/DetailFlex.java
@@ -1,6 +1,12 @@
 package dk.au.mad21fall.assignment.sousvideentusiaster.DetailView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,12 +24,15 @@ public class DetailFlex extends AppCompatActivity implements CommentAdapter.ICom
 
     private ArrayList<Comment> commentArrayList;
 
-    public static final int NUM_COMMENTS = 5;
+    public static final int NUM_COMMENTS = 1;
 
     //widgets
     private RecyclerView rcvList;
     private CommentAdapter adapter;
 
+    EditText addComment;
+    ImageView image_profile;
+    TextView post;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,18 +55,47 @@ public class DetailFlex extends AppCompatActivity implements CommentAdapter.ICom
         rcvList = findViewById(R.id.flex_commentSection_recyclerView);
         rcvList.setLayoutManager(new LinearLayoutManager(this));
 
+        addComment = findViewById(R.id.add_comment);
+        image_profile = findViewById(R.id.image_profile);
+        post = findViewById(R.id.post);
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(addComment.getText().toString().equals("")){
+                    Toast.makeText(DetailFlex.this, "You can't send and empty comment", Toast.LENGTH_SHORT).show();
+                }else{
+                    addComment();
+                    addComment.setText("");
+                    InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+                    adapter.updateCommentList(commentArrayList);
+                }
+            }
+        });
+
         rcvList.setAdapter(adapter);
         createData();
         adapter.updateCommentList(commentArrayList);
     }
 
+
+
     private void createData() {
         commentArrayList = new ArrayList<Comment>();
         Random r = new Random();
         for(int i = 0; i < NUM_COMMENTS; i++){
-            commentArrayList.add(new Comment("Matias munkeskider " + i,
+            commentArrayList.add(new Comment("With an extra ImageView we can set the TextView to be baseline aligned with the ImageView and set the android:baselineAlignBottom on the ImageView to true, which will make the baseline of ImageView to bottom." + i,
                     i + " Anton Slimo"
             ));
         }
+    }
+
+    private void addComment() {
+        commentArrayList.add(new Comment(
+                addComment.getText().toString(),
+                "Test publisher"
+        ));
     }
 }
